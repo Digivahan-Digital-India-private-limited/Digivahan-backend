a00e13fd270ed27daa4fc387201b385342a0461c
Saif <hasansaifkhan@gmail.com>
2026-04-23 11:41:35 +0530

    impleament order controller

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -26,7 +26,7 @@ const GenerateOrderByUser = async (req, res) => {
       shipping_is_billing,
       shipping,
       billing,
-      parcel = {},
+      parcel: _parcel,
       order_items,
     } = req.body;
 
@@ -104,12 +104,15 @@ const GenerateOrderByUser = async (req, res) => {
         pincode: billing.pincode,
       },
 
-     parcel: {
-  length: parcel.length ?? 20,
-  breadth: parcel.breadth ?? 15,
-  height: parcel.height ?? 10,
-  weight: parcel.weight ?? 0.05,
-},
+      parcel: (() => {
+        const parcel = _parcel || {};
+        return {
+          length: parcel.length ?? 20,
+          breadth: parcel.breadth ?? 15,
+          height: parcel.height ?? 10,
+          weight: parcel.weight ?? 0.05,
+        };
+      })(),
 
       order_items: order_items.map((item) => ({
         vehicle_id: item.vehicle_id,
